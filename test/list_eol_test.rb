d80b6fea0123192a96f345021477c1bb3b2d00ef
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-08-06 23:26:38 +0200

    Now focusing on the End-of-list implementation

---
+++ test/list_eol_test.rb
@@ -0,0 +1,81 @@
+require 'test_helper'
+
+describe Linked::List::EOL do
+  subject { Linked::List::EOL }
+  
+  let(:list) { Minitest::Mock.new }
+  let(:eol) { subject.new list: list }
+  let(:item) { Linked::Item.new }
+  let(:item_a) { Linked::Item.new }
+  let(:item_b) { Linked::Item.new }
+
+  describe '#nil?' do
+    it 'returns true' do
+      assert eol.nil?
+    end
+  end
+
+  describe '#append' do
+    it 'inserts an item when it is empty' do
+      list.expect :increment, nil, [1]
+      eol.append item
+      
+      assert_same item, eol.prev
+      assert_same item, eol.next
+      list.verify
+    end
+    
+    it 'inserts an item when it is not empty' do
+      list.expect :increment, nil, [1]
+      eol.append item_a
+      
+      list.expect :increment, nil, [1]
+      eol.append item_b
+      
+      assert_same item_b, eol.prev
+      assert_same item_a, eol.next
+    end
+    
+    it 'inserts a string of items' do
+      item_a.append item_b
+      list.expect :increment, nil, [2]
+      eol.append item_a
+      
+      assert_same item_b, eol.prev
+      assert_same item_a, eol.next
+      list.verify
+    end
+  end
+  
+  describe '#prepend' do
+    it 'inserts an item when it is empty' do
+      list.expect :increment, nil, [1]
+      eol.prepend item
+      
+      assert_same item, eol.prev
+      assert_same item, eol.next
+      list.verify
+    end
+    
+    it 'inserts an item when it is not empty' do
+      list.expect :increment, nil, [1]
+      eol.prepend item_b
+      
+      list.expect :increment, nil, [1]
+      eol.prepend item_a
+      
+      assert_same item_b, eol.prev
+      assert_same item_a, eol.next
+    end
+    
+    it 'inserts a string of items' do
+      item_b.prepend item_a
+      list.expect :increment, nil, [2]
+      eol.prepend item_b
+      
+      assert_same item_b, eol.prev
+      assert_same item_a, eol.next
+      list.verify
+    end
+  end
+end
