5f7edbcab532a423bfd34b78d6333035ac5453c4
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-08-12 14:48:55 +0200

    Restructured the tests so that all interactions with lists are separate

--- test/listable_test.rb
+++ test/listable_test.rb
@@ -15,26 +15,15 @@ describe Linked::Listable do
   subject { ::Item }
 
   let(:item) { subject.new }
-  let(:item_in_list) { subject.new.tap { |s| list.append s } }
+  
   let(:item_a) { subject.new }
   let(:item_b) { subject.new }
   let(:item_c) { subject.new }
 
   let(:sibling) { subject.new }
-
-  let(:head) { Minitest::Mock.new }
-  let(:tail) { Minitest::Mock.new }
-  let(:list) do
-    mock = Minitest::Mock.new
-    #mock.expect :tail, tail
-    mock.expect(:append, nil) do |item|
-      item.send :list=, mock
-      item.send :next=, tail
-      item.send :prev=, head
-      true
-    end
-    mock
-  end
+  
+  # The following tests all describe the behaviour of a Listable item outside of
+  # a list. Items within lists are tested further down below.
 
   describe '#item' do
     it 'returns the item itself' do
@@ -43,21 +32,13 @@ describe Linked::Listable do
   end
 
   describe '#list' do
-    it 'returns the list the item is part of' do
-      assert_same list.object_id, item_in_list.list.object_id
-    end
-
-    it 'raises an exception when the item is not in a list' do
+    it 'raises a NoMethodError' do
       assert_raises(NoMethodError) { item.list }
     end
   end
 
   describe '#in_list?' do
-    it 'returns true when the item is in a list' do
-      assert item_in_list.in_list?
-    end
-
-    it 'returns false when the item is not in a list' do
+    it 'returns false' do
       refute item.in_list?
     end
   end
@@ -72,12 +53,6 @@ describe Linked::Listable do
       item.prepend sibling
       refute item.first?
     end
-
-    it 'returns true when first in a list' do
-      head.expect :nil?, true
-      assert item_in_list.first?
-      head.verify
-    end
   end
 
   describe '#last?' do
@@ -90,23 +65,11 @@ describe Linked::Listable do
       item.append sibling
       refute item.last?
     end
-
-    it 'returns true when lasy in a list' do
-      tail.expect :nil?, true
-      assert item_in_list.last?
-      tail.verify
-    end
   end
 
   describe '#in?' do
-    it 'returns true if the item is in the list' do
-      list.expect :equal?, true, [list]
-      #item = subject.new list: list
-      list.append item
-      assert item.in?(list)
-    end
-
-    it 'returns false if the item is not in the list' do
+    it 'returns false if the item is not in a list' do
+      list = Minitest::Mock.new
       refute item.in?(list)
     end
   end
@@ -168,8 +131,10 @@ describe Linked::Listable do
     end
 
     it 'accepts any object responding to #item' do
-      list.expect :item, item_b
-      item_a.append list
+      object = Minitest::Mock.new
+      object.expect :item, item_b
+      item_a.append object
+      assert_same item_b, item_a.next
     end
 
     it 'only inserts the items after the given one' do
@@ -182,50 +147,6 @@ describe Linked::Listable do
       assert_same item_b, item.next
     end
 
-    it 'calls #prev= on tail and #grow on the list when last in one' do
-      list.expect :grow, nil, [1]
-      tail.expect :prev=, nil, [sibling]
-
-      item_in_list.append sibling
-
-      tail.verify
-      list.verify
-    end
-
-    it 'inserts multiple connected items when in a list' do
-      list.expect :grow, nil, [2]
-      tail.expect :prev=, nil, [item_c]
-
-      item_b.append item_c
-      item_in_list.append item_b
-
-      list.verify
-      tail.verify
-
-      assert_equal list.object_id, item_c.list.object_id
-    end
-
-    it 'removes items already in a list from that list' do
-      tail.expect :prev=, nil, [item_b]
-      list.expect :grow, nil, [2]
-
-      item_a.append item_b
-      item_in_list.append item_a # grows the list by 2
-
-      list.verify && tail.verify
-
-      list.expect :shrink, nil, [2]
-      tail.expect :nil?, true
-      tail.expect :prev=, nil, [item_in_list]
-
-      item.append item_a
-
-      refute item.in_list?
-      refute item_a.in_list?
-
-      list.verify && tail.verify
-    end
-
     it 'accepts an argument' do
       item.append :argument
 
@@ -233,23 +154,11 @@ describe Linked::Listable do
       assert_equal [:argument], item.next.args
     end
     
-    it 'accepts any number of arguments' do
+    it 'accepts an arbitrary object' do
       item.append :argument
   
       assert_kind_of subject, item.next
       assert_equal [:argument], item.next.args
-    end
-
-    it 'asks the list to create the item when given a value' do
-      item_in_list && list.verify
-      
-      list.expect :create_item, item, [:value]
-      list.expect :grow, nil
-      tail.expect :prev=, nil, [item]
-
-      item_in_list.append :value
-
-      list.verify
     end
   end
 
@@ -282,8 +191,10 @@ describe Linked::Listable do
     end
 
     it 'accepts any object responding to #item' do
-      list.expect :item, item_b
-      item_a.prepend list
+      object = Minitest::Mock.new
+      object.expect :item, item_b
+      item_a.prepend object
+      assert_same item_b, item_a.prev
     end
 
     it 'only inserts the items before the given one' do
@@ -296,67 +207,11 @@ describe Linked::Listable do
       assert_same item_b, item.prev
     end
 
-    it 'calls #next= on head and #incerment on the list when first in one' do
-      list.expect :grow, nil, [1]
-      head.expect :next=, nil, [sibling]
-      head.expect :nil?, true
-
-      item_in_list.prepend sibling
-      assert sibling.first?
-
-      head.verify
-      list.verify
-    end
-
-    it 'inserts multiple connected items when in a list' do
-      list.expect :grow, nil, [2]
-      head.expect :next=, nil, [item_a]
-
-      item_b.prepend item_a
-      item_in_list.prepend item_b
-
-      list.verify
-      head.verify
-
-      assert_equal list.object_id, item_a.list.object_id
-    end
-
-    it 'removes items already in a list from that list' do
-      head.expect :next=, nil, [item_a]
-      list.expect :grow, nil, [2]
-
-      item_a.append item_b
-      item_in_list.prepend item_b # grows the list by 2
-
-      list.verify && head.verify
-
-      list.expect :shrink, nil, [2]
-      head.expect :nil?, true
-      head.expect :next=, nil, [item_in_list]
-
-      item.prepend item_b
-
-      refute item_a.in_list?
-      refute item_b.in_list?
-
-      list.verify && head.verify
-    end
-
-    it 'accepts an argument' do
+    it 'accepts an arbitrary object' do
       item.prepend :argument
       
       assert_kind_of subject, item.prev
       assert_equal [:argument], item.prev.args
-    end
-
-    it 'asks the list to create the item when given a value' do
-      list.expect :create_item, item, [:value]
-      list.expect :grow, nil
-      head.expect :next=, nil, [item]
-
-      item_in_list.prepend :value
-
-      list.verify
     end
   end
 
@@ -393,42 +248,6 @@ describe Linked::Listable do
 
       assert item_c.first?
     end
-
-    it 'calls #next= on head and #shrink on list when first in a list' do
-      # First setup the item chain
-      list.expect :grow, nil, [1]
-      tail.expect :prev=, nil, [sibling]
-      item_in_list.append sibling
-
-      list.expect :shrink, nil
-      head.expect :next=, nil, [sibling]
-      item_in_list.delete
-      head.verify
-      list.verify
-    end
-
-    it 'calls #prev= on tail when last in a list' do
-      # First setup the item chain
-      list.expect :grow, nil, [1]
-      head.expect :next=, nil, [sibling]
-      item_in_list.prepend sibling
-
-      list.expect :shrink, nil
-      tail.expect :prev=, nil, [sibling]
-      item_in_list.delete
-      tail.verify
-      list.verify
-    end
-
-    it 'calls both #next= and #prev= when deleting a single item' do
-      list.expect :shrink, nil
-      head.expect :next=, nil, [tail]
-      tail.expect :prev=, nil, [head]
-      item_in_list.delete
-      head.verify
-      tail.verify
-      list.verify
-    end
   end
 
   describe '#delete_before' do
@@ -446,29 +265,6 @@ describe Linked::Listable do
       assert item_b.last?
       assert item_c.first?
     end
-
-    it 'removes items from a list' do
-      head.expect :next=, nil, [item_a]
-      list.expect :grow, nil, [2]
-
-      item_in_list.prepend item_b # grows the list by 2
-
-      assert item_a.in_list?
-      assert item_b.in_list?
-
-      list.verify && head.verify
-
-      list.expect :shrink, nil, [2]
-      head.expect :nil?, true
-      head.expect :next=, nil, [item_in_list]
-
-      item_in_list.delete_before
-
-      refute item_a.in_list?
-      refute item_b.in_list?
-
-      list.verify && head.verify
-    end
   end
 
   describe '#delete_after' do
@@ -485,29 +281,6 @@ describe Linked::Listable do
       assert_same item_c, item_a.delete_after
       assert item_a.last?
       assert item_b.first?
-    end
-
-    it 'removes items from a list' do
-      tail.expect :prev=, nil, [item_c]
-      list.expect :grow, nil, [2]
-
-      item_in_list.append item_b # grows the list by 2
-
-      assert item_b.in_list?
-      assert item_c.in_list?
-
-      list.verify && tail.verify
-
-      list.expect :shrink, nil, [2]
-      tail.expect :nil?, true
-      tail.expect :prev=, nil, [item_in_list]
-
-      item_in_list.delete_after
-
-      refute item_b.in_list?
-      refute item_c.in_list?
-
-      list.verify && head.verify
     end
   end
 
@@ -555,15 +328,245 @@ describe Linked::Listable do
       assert duped_item.first?
       assert duped_item.last?
     end
+  end
+  
+  # The following tests all apply to listeable items interacting with lists.
+  
+  describe 'in list' do
+    let(:item_in_list) { subject.new.tap { |s| list.append s } }
+    let(:head) { Minitest::Mock.new }
+    let(:tail) { Minitest::Mock.new }
+    let(:list) do
+      mock = Minitest::Mock.new
+      mock.expect(:append, nil) do |item|
+        item.send :list=, mock
+        item.send :next=, tail
+        item.send :prev=, head
+        true
+      end
+      mock
+    end
+    
+    describe '#list' do
+      it 'returns the list' do
+        assert_same list.object_id, item_in_list.list.object_id
+      end
+    end
 
-    it 'disconects the new item from its list' do
-      list.expect :grow, nil, [2]
-      tail.expect :prev=, nil, [item_c]
+    describe '#in_list?' do
+      it 'returns true' do
+        assert item_in_list.in_list?
+      end
+    end
 
-      item_b.append item_c
-      item_in_list.append item_b
+    describe '#first?' do
+      it 'returns true when first' do
+        head.expect :nil?, true
+        assert item_in_list.first?
+        head.verify
+      end
+    end
 
-      refute item_b.dup.in_list?
+    describe '#last?' do
+      it 'returns true when last' do
+        tail.expect :nil?, true
+        assert item_in_list.last?
+        tail.verify
+      end
+    end
+
+    describe '#in?' do
+      it 'returns true if the item is in the list' do
+        list.expect :equal?, true, [list]
+        list.append item
+        assert item.in?(list)
+      end
+    end
+
+    describe '#append' do
+      it 'calls #prev= on tail and #grow on the list' do
+        list.expect :grow, nil, [1]
+        tail.expect :prev=, nil, [sibling]
+
+        item_in_list.append sibling
+
+        tail.verify
+        list.verify
+      end
+      
+      it 'removes items already in a list from that list' do
+        tail.expect :prev=, nil, [item_b]
+        list.expect :grow, nil, [2]
+    
+        item_a.append item_b
+        item_in_list.append item_a # grows the list by 2
+    
+        list.verify && tail.verify
+    
+        list.expect :shrink, nil, [2]
+        tail.expect :nil?, true
+        tail.expect :prev=, nil, [item_in_list]
+    
+        item.append item_a
+    
+        refute item.in_list?
+        refute item_a.in_list?
+    
+        list.verify && tail.verify
+      end
+
+      it 'inserts multiple connected items' do
+        list.expect :grow, nil, [2]
+        tail.expect :prev=, nil, [item_c]
+
+        item_b.append item_c
+        item_in_list.append item_b
+
+        list.verify
+        tail.verify
+
+        assert_equal list.object_id, item_c.list.object_id
+      end
+
+      it 'asks the list to create an item when given an object' do
+        item_in_list && list.verify
+
+        list.expect :create_item, item, [:value]
+        list.expect :grow, nil
+        tail.expect :prev=, nil, [item]
+
+        item_in_list.append :value
+
+        list.verify
+      end
+    end
+
+    describe '#prepend' do
+      it 'calls #next= on head and #grow on the list' do
+        list.expect :grow, nil, [1]
+        head.expect :next=, nil, [sibling]
+        head.expect :nil?, true
+
+        item_in_list.prepend sibling
+        assert sibling.first?
+
+        head.verify
+        list.verify
+      end
+      
+      it 'removes items already in a list from that list' do
+        head.expect :next=, nil, [item_a]
+        list.expect :grow, nil, [2]
+    
+        item_a.append item_b
+        item_in_list.prepend item_b # grows the list by 2
+    
+        list.verify && head.verify
+    
+        list.expect :shrink, nil, [2]
+        head.expect :nil?, true
+        head.expect :next=, nil, [item_in_list]
+    
+        item.prepend item_b
+    
+        refute item_a.in_list?
+        refute item_b.in_list?
+    
+        list.verify && head.verify
+      end
+
+      it 'inserts multiple connected items' do
+        list.expect :grow, nil, [2]
+        head.expect :next=, nil, [item_a]
+
+        item_b.prepend item_a
+        item_in_list.prepend item_b
+
+        list.verify
+        head.verify
+
+        assert_equal list.object_id, item_a.list.object_id
+      end
+
+      it 'asks the list to create an item when given an object' do
+        list.expect :create_item, item, [:value]
+        list.expect :grow, nil
+        head.expect :next=, nil, [item]
+
+        item_in_list.prepend :value
+
+        list.verify
+      end
+    end
+    
+    describe 'deleting' do
+      before do
+        list.expect :grow, nil, [1]
+        head.expect :next=, nil, [item_a]
+        
+        item_in_list.prepend item_a
+        list.verify && head.verify
+        
+        list.expect :grow, nil, [1]
+        tail.expect :prev=, nil, [item_b]
+        
+        item_in_list.append item_b
+        list.verify && tail.verify
+      end
+
+      describe '#delete' do
+        it 'calls #shrink on the list' do
+          list.expect :shrink, nil
+          item_in_list.delete
+          list.verify
+        end
+        
+        it 'calls #next= on head when first' do
+          list.expect :shrink, nil
+          head.expect :next=, nil, [item_in_list]
+          item_a.delete
+          head.verify && list.verify
+        end
+  
+        it 'calls #prev= on tail when last' do
+          list.expect :shrink, nil
+          tail.expect :prev=, nil, [item_in_list]
+          item_b.delete
+          tail.verify && list.verify
+        end
+      end
+  
+      describe '#delete_before' do
+        it 'removes items from a list' do
+          list.expect :shrink, nil, [2]
+          head.expect :nil?, true
+          head.expect :next=, nil, [item_b]
+  
+          item_b.delete_before
+  
+          refute item_a.in_list?
+          list.verify && head.verify
+        end
+      end
+  
+      describe '#delete_after' do
+        it 'removes items from a list' do
+          list.expect :shrink, nil, [2]
+          tail.expect :nil?, true
+          tail.expect :prev=, nil, [item_a]
+  
+          item_a.delete_after
+  
+          refute item_b.in_list?
+          list.verify && head.verify
+        end
+      end
+    end
+
+    describe '#dup' do
+      it 'disconects the new item from its list' do
+        refute item_in_list.dup.in_list?
+      end
     end
   end
 end
